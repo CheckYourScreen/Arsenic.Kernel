755eb46b0c09a0dad26a82de5ff523c7deb7aa99
CheckYourScreen <fictitiousguy@yahoo.com>
2016-07-19 09:54:03 +0200

    Add and Enable zzmoove gov.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -388,6 +388,9 @@ extern struct cpufreq_governor cpufreq_gov_wheatley;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTMAX)
 extern struct cpufreq_governor cpufreq_gov_smartmax;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_smartmax)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ZZMOOVE)
+extern struct cpufreq_governor cpufreq_gov_zzmoove;
+#define CPUFREQ_DEFAULT_GOVERNOR       (&cpufreq_gov_zzmoove)
 #endif
 
 
