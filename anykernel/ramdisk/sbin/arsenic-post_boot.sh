526e17a951e073e3e1bb2c6ec5271e4a45bf69d3
CheckYourScreen <fictitiousguy@yahoo.com>
2016-07-26 16:12:11 +0200

    Add Build Script and Required Files.
    dont forget to enter version no. for naming convention of zip while executing the zip.
    
    Signed-off-by: CheckYourScreen <fictitiousguy@yahoo.com>

---
+++ anykernel/ramdisk/sbin/arsenic-post_boot.sh
@@ -0,0 +1,12 @@
+#!/system/bin/sh
+
+############################
+# Custom Kernel Settings for Tyr Kernel by DerRomtester!!
+# Credits to RenderBroken
+# Modified by Nimit Mehta
+echo "Boot Script Started" | tee /dev/kmsg
+
+# Dummy File
+
+
+echo "[Tyr-Kernel] Boot Script Completed!" | tee /dev/kmsg
---
+++ arsenic.sh
@@ -0,0 +1,58 @@
+#For Time Calculation
+BUILD_START=$(date +"%s")
+echo "enter version name for zip name (only number) :" 
+read VER
+# Housekeeping
+KERNEL_DIR=$PWD
+KERN_IMG=$KERNEL_DIR/arch/arm/boot/zImage
+KERN_DTB=$KERNEL_DIR/arch/arm/boot/dt.img
+OUT_DIR=$KERNEL_DIR/anykernel/
+
+blue='\033[0;34m'
+cyan='\033[0;36m'
+yellow='\033[0;33m'
+red='\033[0;31m'
+nocol='\033[0m'
+
+make clean && make mrproper
+export ARCH=arm
+export CROSS_COMPILE="/home/nimit/cmgcc4.9/bin/arm-linux-androideabi-"
+
+compile_kernel ()
+{
+echo -e "**********************************************************************************************"
+echo "                    "
+echo "                                    Compiling Arsenic-Kernel                    "
+echo "                    "
+echo -e "**********************************************************************************************"
+make onyx_defconfig
+make -j16
+if ! [ -a $KERN_IMG ];
+then
+echo -e "$red Kernel Compilation failed! Fix the errors! $nocol"
+exit 1
+fi
+dtb
+zipping
+}
+
+dtb() {
+tools_sk/dtbtool -o $KERN_DTB -s 2048 -p $KERNEL_DIR/scripts/dtc/ $KERNEL_DIR/arch/arm/boot/
+
+}
+
+
+zipping() {
+rm -rf $OUT_DIR/arsenic*.zip
+rm -rf $OUT_DIR/zImage
+rm -rf $OUT_DIR/dtb
+cp $KERN_IMG $OUT_DIR/zImage
+cp $KERN_DTB $OUT_DIR/dtb
+cd $OUT_DIR
+zip -r arsenic.kernel-onyx.R$VER-$(date +"%Y%m%d").zip *
+}
+
+compile_kernel
+BUILD_END=$(date +"%s")
+DIFF=$(($BUILD_END - $BUILD_START))
+echo -e "$yellow Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$nocol"
