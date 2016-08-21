ed488d81e86448ff67677f2083fcd2d383579cad
CheckYourScreen <fictitiousguy@yahoo.com>
2016-08-21 06:59:10 +0200

    Revert "rtmutex: Confine deadlock logic to futex"
    This reverts commit 90a1aa2ee5b10bc6aedcc4ca88e77f459b2ba9e3.

--- kernel/rtmutex_common.h
+++ kernel/rtmutex_common.h
@@ -110,11 +110,12 @@ extern void rt_mutex_proxy_unlock(struct rt_mutex *lock,
 				  struct task_struct *proxy_owner);
 extern int rt_mutex_start_proxy_lock(struct rt_mutex *lock,
 				     struct rt_mutex_waiter *waiter,
-				     struct task_struct *task);
+				     struct task_struct *task,
+				     int detect_deadlock);
 extern int rt_mutex_finish_proxy_lock(struct rt_mutex *lock,
 				      struct hrtimer_sleeper *to,
-				      struct rt_mutex_waiter *waiter);
-extern int rt_mutex_timed_futex_lock(struct rt_mutex *l, struct hrtimer_sleeper *to);
+				      struct rt_mutex_waiter *waiter,
+				      int detect_deadlock);
 
 #ifdef CONFIG_DEBUG_RT_MUTEXES
 # include "rtmutex-debug.h"
