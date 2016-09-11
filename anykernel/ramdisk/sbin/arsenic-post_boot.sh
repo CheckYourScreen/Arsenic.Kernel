6fb69ac7c7ae565a7134e4957d29ada69cd9c94a
Nimit Mehta <fictitiousguy@yahoo.com>
2016-09-11 11:47:11 +0530

    Boot Script: Cleanup

--- anykernel/ramdisk/sbin/arsenic-post_boot.sh
+++ anykernel/ramdisk/sbin/arsenic-post_boot.sh
@@ -1,12 +1,12 @@
 #!/system/bin/sh
 
 ############################
-# Custom Kernel Settings for Tyr Kernel by DerRomtester!!
-# Credits to RenderBroken
+# Custom Kernel Settings for Arsenic Kernel
+# Credits to RenderBroken & DerRomtester
 # Modified by Nimit Mehta
 echo "Boot Script Started" | tee /dev/kmsg
 
-# Dummy File
+# Dummy File for Future use (if any)
 
 
-echo "[Tyr-Kernel] Boot Script Completed!" | tee /dev/kmsg
+echo "[Arsenic-Kernel] Boot Script Completed!" | tee /dev/kmsg
