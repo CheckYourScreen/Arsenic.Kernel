75d822ec51bca9685e19dce54abe9ba6b1594605
CheckYourScreen <nimitmehta95@gmail.com>
2016-11-21 19:42:38 +0100

    Add and Enable Modified ElementalX Governor
    Signed-off-by: CheckYourScreen <nimitmehta95@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -392,6 +392,9 @@ extern struct cpufreq_governor cpufreq_gov_smartmax;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ZZMOOVE)
 extern struct cpufreq_governor cpufreq_gov_zzmoove;
 #define CPUFREQ_DEFAULT_GOVERNOR       (&cpufreq_gov_zzmoove)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ELEMENTALX)
+extern struct cpufreq_governor cpufreq_gov_elementalx;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_elementalx)
 #endif
 
 
