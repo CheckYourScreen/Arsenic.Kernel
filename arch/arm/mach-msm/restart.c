24364be11e77a4228021a9a44f78ca5d18852a6b
CheckYourScreen <nimitmehta95@gmail.com>
2017-02-28 14:16:25 +0100

    arm: disable download mode
    Device crashes into download mode (black screen of death with white LED) in case of panic.
    This disables download mode to make device restart automatically whenever an exception occurs.
    
    Signed-off-by: CheckYourScreen <nimitmehta95@gmail.com>

--- arch/arm/mach-msm/restart.c
+++ arch/arm/mach-msm/restart.c
@@ -74,16 +74,7 @@ static void *emergency_dload_mode_addr;
 
 /* Download mode master kill-switch */
 static int dload_set(const char *val, struct kernel_param *kp);
-#ifndef CONFIG_VENDOR_EDIT  
-//He Wei@OnLineRD,  2013/08/02, modify for reboot after crash
-static int download_mode = 1;
-#else      /*CONFIG_VENDOR_EDIT*/
-#ifndef RELEASE_DOWNLOAD_MODE_SET
-static int download_mode = 1;
-#else
 static int download_mode = 0;
-#endif   /*CONFIG_VENDOR_EDIT*/
-#endif
 module_param_call(download_mode, dload_set, param_get_int,
 			&download_mode, 0644);
 static int panic_prep_restart(struct notifier_block *this,
